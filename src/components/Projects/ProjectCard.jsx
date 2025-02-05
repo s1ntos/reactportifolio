fc1cd705abce88055d4bcefc0923341f91f7ba04
João <109487647+s1ntos@users.noreply.github.com>
2025-02-04 21:06:26 -0300

    Update ProjectCard.jsx

--- src/components/Projects/ProjectCard.jsx
+++ src/components/Projects/ProjectCard.jsx
@@ -4,7 +4,7 @@ import styles from "./ProjectCard.module.css";
 import { getImageUrl } from "../../utils";
 
 export const ProjectCard = ({
-  project: { title, imageSrc, description, skills, demo, source },
+  project: { title, imageSrc, description, skills, source },
 }) => {
   return (
     <div className={styles.container}>
